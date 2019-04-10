cb335262a8cb4225b2896f21f803a1fa8b779a42
BOSS <1329540850@qq.com>
2019-04-10 21:20:21 +0800

    first test

---
+++ src/main/java/cn/ljlin233/config/LabWebAppInitializer.java
@@ -0,0 +1,27 @@
+package cn.ljlin233.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * LabWebAppInitializer
+ */
+public class LabWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    // SpringApplicationContext
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {RootConfig.class};
+    }
+
+    // SpringMVCApplicationContext
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+
+}
---
+++ src/main/java/cn/ljlin233/config/RootConfig.java
@@ -0,0 +1,14 @@
+package cn.ljlin233.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RootConfig
+ */
+@Configuration
+@ComponentScan(basePackages = {"cn.ljlin233.config"})
+public class RootConfig {
+
+    
+}
