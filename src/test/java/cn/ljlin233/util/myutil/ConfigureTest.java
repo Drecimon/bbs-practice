d85e397453f57690f898190af039da2d4567bbed
BOSS <1329540850@qq.com>
2019-05-09 17:23:27 +0800

    文件上传功能

--- src/test/java/cn/ljlin233/util/myutil/ConfigureTest.java
+++ src/test/java/cn/ljlin233/util/myutil/ConfigureTest.java
@@ -3,21 +3,8 @@ package cn.ljlin233.util.myutil;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
-import cn.ljlin233.config.RootConfig;
-import cn.ljlin233.config.WebConfig;
 
-/**
- * ConfigureTest
- */
-//@RunWith(SpringJUnit4ClassRunner.class)
-// @ContextConfiguration(classes = { RootConfig.class, WebConfig.class })
-// @WebAppConfiguration
 public class ConfigureTest {
 
     @Test
